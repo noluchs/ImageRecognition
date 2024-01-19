8099fdd10754095bbad86df72c753d6f00610b41
Noah <74672861+noluchs@users.noreply.github.com>
2024-01-19 19:07:41 +0100

    update

--- code/uploadS3/upload.py
+++ code/uploadS3/upload.py
@@ -2,6 +2,10 @@ import os
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import boto3
+from dotenv import load_dotenv
+
+load_dotenv("s3-acceskey.env")  # Name Ihrer Umgebungsvariablendatei
+
 
 app = Flask(__name__)
 s3 = boto3.client("s3",
@@ -16,7 +20,7 @@ def upload_file():
         filename = secure_filename(file.filename)
         s3.upload_fileobj(file, 'imagerekognition-s3-upload', filename)
         return 'File uploaded successfully'
-    return render_template('upload.html')
+    return render_template('templates/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
